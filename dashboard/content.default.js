377907da6675e4ae53aed6cbde6a5c2ecc663272
akolites <akolites@users.noreply.github.com>
2022-06-13 12:59:16 +0700

    add basic flow

---
+++ dashboard/content.default.js
@@ -0,0 +1,92 @@
+var lower3rdContents = [
+    {
+        id: 'laguPembuka',
+        content: {
+            h1: "Pembuka", h2: ""
+        }
+    },
+    {
+        id: 'laguTuhanKasihanilahKami',
+        content: {
+            h1: "Tuhan, Kasihanilah Kami", h2: ""
+        }
+    },
+    {
+        id: 'laguKemuliaan',
+        content: {
+            h1: "Kemuliaan", h2: ""
+        }
+    },
+    {
+        id: 'bacaanPertama',
+        content: {
+            h1: "Bacaan Pertama", h2: ""
+        }
+    },
+    {
+        id: 'mazmurTanggapan',
+        content: {
+            h1: "Mazmur Tanggapan", h2: ""
+        }
+    },
+    {
+        id: 'bacaanKedua',
+        content: {
+            h1: "Bacaan Kedua", h2: ""
+        }
+    },
+    {
+        id: 'baitPengantarInjil',
+        content: {
+            h1: "Bait Pengantar Injil", h2: ""
+        }
+    },
+    {
+        id: 'bacaanInjil',
+        content: {
+            h1: "Bacaan Injil", h2: ""
+        }
+    },
+    {
+        id: 'laguPersembahan',
+        content: {
+            h1: "Persembahan", h2: ""
+        }
+    },
+    {
+        id: 'laguKudus',
+        content: {
+            h1: "Sanctus", h2: ""
+        }
+    },
+    {
+        id: 'laguBapaKami',
+        content: {
+            h1: "Bapa Kami", h2: ""
+        }
+    },
+    {
+        id: 'laguAnakDombaAllah',
+        content: {
+            h1: "Anak Domba Allah", h2: ""
+        }
+    },
+    {
+        id: 'laguKomuni',
+        content: {
+            h1: "Komuni", h2: ""
+        }
+    },
+    {
+        id: 'laguPenutup',
+        content: {
+            h1: "Penutup", h2: ""
+        }
+    },
+    {
+        id: 'selebran',
+        content: {
+            h1: "Selebran", h2: ""
+        }
+    },
+];
---
+++ dashboard/kalender.js
@@ -0,0 +1,58 @@
+/**
+ * Get liturgy  of the day from akolites/kalender-liturgi
+ * 
+ * @param {Date} date 
+ * @returns Object liturgy of the day
+ */
+async function getLitDay(date) {
+    const monthYear = `${date.getFullYear()}-${`0${date.getMonth() + 1}`.slice(-2)}`; //yyyy-MM
+    const litDay = await fetch(`https://raw.githubusercontent.com/akolites/kalender-liturgi/main/api/v1/${monthYear}.json`)
+        .then(res => res.json()
+        ).then(k => k[date.getDate() - 1]);
+    return litDay;
+};
+
+/**
+ * Merge all readings in 'q' parameter to one
+ * 
+ * @param {Array<URL>} urls 
+ * @returns URL 
+ */
+function mergeReadingURLs(urls) {
+    const newURL = urls[0];
+    const mergedReadings = urls.map(u => u.searchParams.get("q"));
+    newURL.searchParams.set("q", mergedReadings.join(';'));
+    return newURL;
+};
+
+/**
+ * Assign readings of the day by guessing
+ * WARNING : Use for general usage like ordinary (harian) / sunday (minggu)
+ * Not applicable for Holy Days (Easter/Christmas).
+ * 
+ * @param {Object} litDay
+ * @returns Object readings of the day
+ */
+function getReadings(litDay) {
+    let readings = litDay.readings.links;
+    readings = readings.filter(v => !v.text.includes("BcO")); // skip BcO
+
+    let result = {};
+    let count = 1;
+    readings.forEach(element => {
+        if (element.text.includes("MT") ||
+            element.text.includes("Mzm")) {
+            result["mazmur"] = element;
+        } else if (element.text.includes("Mat")
+            || element.text.includes("Mrk")
+            || element.text.includes("Luk")
+            || element.text.includes("Yoh")) {
+            result["injil"] = element;
+        } else {
+            result[`bacaan${count}`] = element;
+            count++;
+        }
+    });
+
+    return result;
+}
